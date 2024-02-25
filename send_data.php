684b765182d2ecfcc47dbba67330e559cae2740d
MARIGANOS <shimizumaribusiness@gmail.com>
2024-02-26 02:02:06 +0900

    いらないとこ削除

--- send_data.php
+++ send_data.php
@@ -7,26 +7,26 @@
     <body>
     <?php
 
-    $gyou=array('a','b','c','d');//人 $i
+    $gyou=array('田中','佐藤','阿部','高橋');//人 $i
     $retu=array(1,2,3,4,5,6,7,8,9,10);//日付 $j
-    $shokichi=array(0,0,0,0,0,0,0,0,0,0);
+
 
     print '<table>';
 //*-----._表を表示させる_.-----*
+print'<form method="post" action="data_send_check.php">';
     for($i=0; $i<count($gyou); $i++){
-        ${$gyou[$i]}=array();//i番目の人の名前の配列を作る1⃣
+        //${$gyou[$i]}=array();//i番目の人の名前の配列を作る1⃣
         print '<tr><td>'.$gyou[$i].'</td>';
         for($j=0;$j<count($retu);$j++){
-            array_push(${$gyou[$i]},0);//1⃣で作った配列に初期値0をpushする
+            //array_push(${$gyou[$i]},0);//1⃣で作った配列に初期値0をpushする
             print '<td>';
-            print'<form method="post" action="data_send_check.php">';
             print'<select name='.$gyou[$i].'[]>';
             print'<option value="0">　</option>';
-            print'<option value="1">あ</option>';
-            print'<option value="2">い</option>';
-            print'<option value="3">う</option>';
-            print'<option value="4">え</option>';
-            print'<option value="5">お</option>';
+            print'<option value="1">早</option>';
+            print'<option value="2">日</option>';
+            print'<option value="3">遅</option>';
+            print'<option value="4">夜</option>';
+            print'<option value="5">休</option>';
             print'</select>';
             print'</td>';
         }
